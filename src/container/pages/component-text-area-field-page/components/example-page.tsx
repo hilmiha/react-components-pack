374f9578a8d0f6390b6cc988fdcda33c72faea33
hilmiha <hilmihidayatar@gmail.com>
2024-05-20 11:02:55 +0700

    - add type for text area [text, text-no-space, text-only-number]

--- src/container/pages/component-text-area-field-page/components/example-page.tsx
+++ src/container/pages/component-text-area-field-page/components/example-page.tsx
@@ -8,6 +8,8 @@ import TextAreaField from "../../../../components/text-area-field";
 
 export type formType = {
     textAreaField:string
+    textNoSpaceAreaField:string
+    textFullNumberAreaField:string
 }
 
 const ExamplePage = () =>{
@@ -16,7 +18,9 @@ const ExamplePage = () =>{
     } = useContext(LocalContext) as LocalContextType;
 
     const [form, setForm] = useState<formType>({
-        textAreaField:''
+        textAreaField:'',
+        textNoSpaceAreaField:'',
+        textFullNumberAreaField:''
     })
 
     const [formError, setFormError] = useState<Record<keyof formType, errorType>>(generateErrorState(form))
@@ -38,7 +42,7 @@ const ExamplePage = () =>{
     return(
         <div className="tab-content">
             <div className="component-section">
-                <span className="font-title">Default</span>
+                <span className="font-title">Text</span>
                 <div className="preview-box">
                     <TextAreaField
                         type="text"
@@ -48,6 +52,40 @@ const ExamplePage = () =>{
                         error={formError['textAreaField']}
                         onChange={(newValue)=>{onChange('textAreaField', newValue)}}
                         onValidate={(errorResult)=>{onValidate('textAreaField', errorResult)}}
+                        config={{
+                            isMandatory:true
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="component-section">
+                <span className="font-title">No Space</span>
+                <div className="preview-box">
+                    <TextAreaField
+                        type="text-no-space"
+                        txtLabel="Form Text Area Label"
+                        txtPlaceholder="Form placeholder..."
+                        value={form['textNoSpaceAreaField']}
+                        error={formError['textNoSpaceAreaField']}
+                        onChange={(newValue)=>{onChange('textNoSpaceAreaField', newValue)}}
+                        onValidate={(errorResult)=>{onValidate('textNoSpaceAreaField', errorResult)}}
+                        config={{
+                            isMandatory:true
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="component-section">
+                <span className="font-title">Full Number</span>
+                <div className="preview-box">
+                    <TextAreaField
+                        type="text-only-number"
+                        txtLabel="Form Text Area Label"
+                        txtPlaceholder="Form placeholder..."
+                        value={form['textFullNumberAreaField']}
+                        error={formError['textFullNumberAreaField']}
+                        onChange={(newValue)=>{onChange('textFullNumberAreaField', newValue)}}
+                        onValidate={(errorResult)=>{onValidate('textFullNumberAreaField', errorResult)}}
                         config={{
                             isMandatory:true
                         }}
